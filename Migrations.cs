bc5cd23d953cac257bc74a61b5bbb2e45f2104b0
Jack Cheng <jack.cheng@commercan.com>
2014-07-16 11:28:35 -0700

    WIP: creating tables

--- Migrations.cs
+++ Migrations.cs
@@ -10,7 +10,18 @@ namespace Commercan.GoogleAnalytics
     public class Migrations : DataMigrationImpl
     {
         public int Create() {
-            
+            SchemaBuilder.CreateTable("SettingsRecord",
+                table => table.Column<int>("Id", column => column.PrimaryKey().Identity())
+                    .Column<bool>("Enabled", column => column.NotNull().WithDefault(false))
+                    .Column<bool>("UseUniversalTracking", column => column.NotNull().WithDefault(false))
+                    .Column<string>("DomainName")
+                    .Column<string>("GoogleAnalyticsKey"));
+
+            SchemaBuilder.CreateTable("ScriptCodesRecord",
+                table => table.Column<int>("Id", column => column.PrimaryKey().Identity())
+                        .Column<string>("ScriptType", column => column.NotNull().WithLength(50))
+                        .Column<string>("Script", column => column.NotNull().Unlimited()));
+            return 1;
         }
     }
 }
