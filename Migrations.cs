c0f58f6b15213665c5ebf0a8ecadd957d5393a57
Jack Cheng <jack.cheng@commercan.com>
2014-07-16 10:52:04 -0700

    initial commit

---
+++ Migrations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Data.Migration;
+using Orchard.Data.Migration.Schema;
+
+namespace Commercan.GoogleAnalytics
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create() {
+            
+        }
+    }
+}
